d6e876ff3e57422c942b3f3518e06c71ece59e7b
Yutong Jin <richiejyt@gmail.com>
2026-08-16 09:46:17 +1200

    Initial Commit

---
+++ FORPETS/FORPETSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FORPETSApp.swift
+//  FORPETS
+//
+//  Created by R1chi3 on 16/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct FORPETSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
